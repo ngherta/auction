348e624b9b38f4bc7b5439c0e606cd03f09ad1dc
INTHER\ngherta <ngherta@inthergroup.com>
2021-10-24 14:05:41 +0300

    implemented logging in project

--- Backend/src/main/java/com/auction/helper/ScheduledTasks.java
+++ Backend/src/main/java/com/auction/helper/ScheduledTasks.java
@@ -3,26 +3,28 @@ package com.auction.helper;
 import com.auction.repository.AuctionEventRepository;
 import com.auction.service.interfaces.AuctionEventService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class ScheduledTasks {
 
   private final AuctionEventService auctionEventService;
   private final AuctionEventRepository auctionEventRepository;
 
-  @Scheduled(fixedDelay = 500)
+  @Scheduled(fixedDelay = 2000)
   public void checkEventForFinish() {
     List<Long> list = auctionEventRepository.getListForChangeStatus();
-    System.out.println("ScheduledTasks.checkEventForFinish: Try to find auction events to finish it.");
+    log.info("Try to find auction events to finish it.");
     if (!list.isEmpty()) {
-      System.out.println("ScheduledTasks.checkEventForFinish: Found auction events for finishing:");
+      log.info("Found auction events for finishing:");
       for (Long id : list) {
-        System.out.println("ScheduledTasks.checkEventForFinish: [" + id + "] try to finish it.");
+        log.info("[" + id + "] try to finish it.");
       }
       auctionEventService.changeStatusToFinished(list);
     }
