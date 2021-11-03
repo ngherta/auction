c471f6afd01ccce635b8758b02672409e7b4fec7
gogo6ar <gherta.micolai@gmail.com>
2021-11-03 21:35:18 +0200

    Create Entity and repository for Payment

---
+++ Backend/src/main/java/com/auction/model/Payment.java
@@ -0,0 +1,59 @@
+package com.auction.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "auction_winner")
+@NoArgsConstructor
+@Data
+public class Payment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne
+  @EqualsAndHashCode.Exclude
+  @ToString.Exclude
+  @JoinColumn(name = "user_id", nullable = false, updatable = false)
+  private User user;
+
+  @Column(name = "card_number")
+  private String cardNumber;
+
+  @Column(name = "expiration_date")
+  private String expirationDate;
+
+  @Column(name = "cvv_code")
+  private String cvv;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+
+  public String getExpirationMonth() {
+    return String.valueOf(expirationDate.charAt(0) + expirationDate.charAt(1));
+  }
+
+  public String getExpirationYear() {
+    return String.valueOf(expirationDate.charAt(3) + expirationDate.charAt(4));
+  }
+
+  public void setExpirationDate(String month, String year) {
+    this.expirationDate = month + "/" + year;
+  }
+}
---
+++ Backend/src/main/java/com/auction/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.auction.repository;
+
+import com.auction.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
