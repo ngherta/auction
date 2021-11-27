6a612ddff1511798d7ee23daa8460b973efcf9b5
gogo6ar <gherta.micolai@gmail.com>
2021-11-27 21:18:33 +0200

    create frontend folder and admin

---
+++ untitled/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException {
+        String sDate1="31/12/1998";
+        Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
+        System.out.println(sDate1+"\t"+date1);
+    }
+}
