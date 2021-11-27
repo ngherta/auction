7348eb6f85dbea8e55b0a27fdd1b3e451d266645
gogo6ar <gherta.micolai@gmail.com>
2021-11-27 22:23:08 +0200

    Add some stuff for frontend project

---
+++ Frontend/auction/src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+<nav class="navbar">
+  <div class="container">
+    <ul class="nav navbar-nav">
+      <div></div>
+
+      <li class="nav-item">
+        <router-link
+            class="nav-link"
+            active-class="active"
+            exact
+            :to="{ name: 'home' }"
+        >
+          Home
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link
+            class="nav-link"
+            active-class="active"
+            exact
+            :to="{ name: 'xxx' }"
+        >
+          Auctions
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link
+            class="nav-link"
+            active-class="active"
+            exact
+            :to="{ name: 'xxx' }"
+        >
+          About us
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link
+            class="nav-link"
+            active-class="active"
+            exact
+            :to="{ name: 'xxx' }"
+        >
+          My profile
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Frontend/auction/src/store/action.type.js
@@ -0,0 +1,5 @@
+export const LOGIN = "login";
+export const LOGOUT = "logout";
+export const REGISTER = "register";
+export const UPDATE_USER = "updateUser";
+export const CHECK_AUTH = "checkAuth";
