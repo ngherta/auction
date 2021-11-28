9e7f1c26b5c762e6cf1b0f7cd9d68f2cf616c19d
gogo6ar <gherta.micolai@gmail.com>
2021-11-28 15:37:49 +0200

    Create a component for svg icons

--- Frontend/auction/src/components/Footer.vue
+++ Frontend/auction/src/components/Footer.vue
@@ -3,15 +3,34 @@
     <div class="container">
       <p>{{footerDescription}}</p>
     </div>
+    <div>
+      <a href="#">
+        <Icon name = 'twitter'></Icon>
+      </a>
+      <a href="#">
+        <Icon name = 'instagram'></Icon>
+      </a>
+      <a href="#">
+        <Icon name = 'facebook'></Icon>
+      </a>
+      <p>Copyright 2021, LOT.<br>
+        All rights reserved.</p>
+    </div>
   </header>
 </template>
 
 <script>
+import Icon from "@/components/svg/Icon";
+
 export default {
   name: "Footer",
+  components: {Icon},
   data() {
     return {
-      footerDescription: 's'
+      footerDescription: 'LOT is a platform for creating events of ' +
+          'buying and selling goods by offering them ' +
+          'up for bid, taking bids, and then selling the item to the ' +
+          'highest bidder or buying the item from the lowest bidder.'
     }
   }
 }
--- Frontend/auction/src/main.js
+++ Frontend/auction/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.component('icon', require('./components/svg/Icon').default);
 
 new Vue({
   render: h => h(App),
