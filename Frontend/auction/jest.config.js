fdcdcf8d00db97e07c2d3e259be6567f31423b5c
ngherta <gherta.nicolai@gmail.com>
2021-12-29 14:33:42 +0200

    Add dependencies for test

--- Frontend/auction/jest.config.js
+++ Frontend/auction/jest.config.js
@@ -1,6 +1,31 @@
 module.exports = {
-    preset: '@vue/cli-plugin-unit-jest',
+    moduleFileExtensions: [
+        'js',
+        'jsx',
+        'json',
+        'vue'
+    ],
     transform: {
-        '^.+\\.vue$': 'vue-jest'
-    }
+        // '^.+\\.vue$': 'vue-jest',
+        '^.+\\.vue$': '@vue/vue3-jest',
+
+        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+        // '^.+\\.jsx?$': 'babel-jest'
+        '^.+\\.[t|j]sx?$': 'babel-jest',
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    snapshotSerializers: [
+        'jest-serializer-vue'
+    ],
+    transformIgnorePatterns: [
+        'node_modules/(?!(ag-grid-vue)/)',
+        'node_modules/(?!(ag-grid-vue3)/)',
+        'node_modules/(?!(@ag-grid-community)/)'
+    ],
+    testMatch: [
+        '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+    ],
+    testURL: 'http://localhost/'
 }
