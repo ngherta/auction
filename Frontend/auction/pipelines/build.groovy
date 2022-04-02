b2a779bd72341272ac18e489f82401218837e882
ngherta <gherta.nicolai@gmail.com>
2022-04-02 22:49:40 +0300

    Folders refactor for mobile

---
+++ Frontend/auction/pipelines/build.groovy
@@ -0,0 +1,62 @@
+pipeline {
+    agent any
+
+    options {
+        disableConcurrentBuilds()
+    }
+    environment {
+        def serviceName = "frontend";
+        def branchName = "main";
+        def versionName = "";
+        def dockerRegistry = "europe-central2-docker.pkg.dev/neat-environs-343619"
+        def imageName = "";
+        def dockerImage = null;
+    }
+
+    stages {
+        stage('Init') {
+            steps {
+                script {
+                    echo 'Init variables'
+
+                    branchName = env.BRANCH_NAME
+                    versionName = env.BRANCH_NAME
+                    imageName = "${serviceName}:${versionName}"
+                }
+
+            }
+        }
+        stage('clone') {
+            steps {
+                script {
+                    echo 'Clone repository'
+
+                    checkout scm
+                }
+            }
+        }
+        stage('build') {
+            steps {
+                script {
+                    echo 'Build docker image'
+                    dir('java/com/') {
+                        dockerImage = docker.build(imageName, , "-f pipelines/Dockerfile .")
+                    }
+
+                }
+            }
+        }
+        stage('docker publish') {
+            steps {
+                script {
+                    echo 'Publish docker image'
+                     docker.withRegistry("https://${dockerRegistry}",  "${serviceName}") {
+
+                        dockerImage.push()
+                     }
+
+                }
+            }
+        }
+    }
+}
