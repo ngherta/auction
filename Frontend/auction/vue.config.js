a8877d6b042343d848850718a73e8af3a696dfc0
Gherta Nicolae <gherta.nicolai@gmail.com>
2022-05-11 14:16:13 +0300

    Remove webpack bundle

--- Frontend/auction/vue.config.js
+++ Frontend/auction/vue.config.js
@@ -1,11 +1,7 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-    .BundleAnalyzerPlugin;
+
 
 module.exports = {
   devServer: {
     port: 8082
-  },
-  configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()]
   }
 }
